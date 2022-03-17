aaea9db2475c131f82db39296a50d61eca85e483
HarutyunHakhverdyan <92093271+HarutyunHakhverdyan@users.noreply.github.com>
2022-03-18 01:45:58 +0400

    Add files via upload

--- aca.js
+++ aca.js
@@ -1,54 +1,100 @@
-//1. Given an array․ Compute the length of the array. (without using .length)
-let arr1 = [1,2,3,4,5,7,,,undefined,,,74,75];
-let length1=0
-for(let key in arr1){
-	length1=+key+1
-};
-// ete  zangvacum bollor tarrer@ dasavorvac en hajordakan ev tarr@ undefind@ chi
-let arr2 = [1,2,3,4,'Hello',65,76,87];
-let length2=0
-for(let i = 0;arr2[i]!=undefined; i++ ){
-	length2++
-};
-console.log(`arr1-${length1}, arr2-${length2}`) 
-//2 Given an array of numbers. Print the sum of the elements in array.
-let arr = [1,8,9,12]
-let sum=0
-for(let i = 0;arr[i]!=undefined; i++ ){
-	sum += arr[i]
-};
-console.log(sum)
-//3. Given two numbers. Print powers of 2 between that numbers. (without using
-//Math.pow)
-let num1 = +prompt();
-let num2 = +prompt();
-for(let i = 0; 2**i<num2; i++ ){
-	if (2**i>num1) {
-	console.log(2**i)
- }
+//1. Given three numbers a, b (a ≤ b) and step. Create an array of evenly spaced elements
+//starting from a to b spaced by step.
+
+function newArr(a,b,step){
+    let arr = []
+    for(let i = a; i<=b; i+=step){
+        arr.push(i)
+    }
+    return arr
 }
-let num = prompt()
-let num1=''
-for(let i = 0; i<num.length; i++){
-	if(!(num[i]%2==0&&num[i+1]%2==0)){
-       num1+=num[i]
-	} else {
-		num1+=num[i]+'-'
-	}
+let result = newArr(1,8,0.5);
+console.log(result);
+//2. Given a string and a symbol. Find the number of occurrences of the symbol in the string.
+
+function getNumSymbol(str,symbol){
+    let num = 0;
+    for(let i = 0;i<str.length; i++){
+        if(str[i]===symbol){
+            num++
+        }
+    }
+    return num
 }
-console.log(num1)
-//5.Insert a n positive number. Print the n-st prime number.
-let num = +prompt()
-let count = 0
-let result
-for(let num1 = 2 ;count!==num; num1++){
-let x=1
-for(let i =2; i<=num1/2+1;i++){
-   x*=num1%i
+let result = getNumSymbol('barev Harut','a');
+console.log(result);
+
+//3. Given a string. Check whether the string is palindrome or not.
+
+function palindrom(str){
+    for(let i = 0; i<str.length; i++){
+        if(str[i]==str[str.length-1-i]){
+            return 'Yes'
+        }else{
+            return 'No'
+        }
+    }
 }
-if(x||num1==2){
-	result=num1
-	++count
- }
+let result = palindrom('ara');
+console.log(result);
+
+//4. Given an array of numbers. Find the maximum element in array.
+
+function arrMax(arr){
+    let max=arr[0]
+    for(let i = 0; i<arr.length;i++){
+        if(max<=arr[i]){
+            max=arr[i]
+        }
+    }
+    return max
 }
+let result = arrMax([37,12,9,1,2,3,7,23]);
+console.log(result);
+//5. Given an array. Create the array which elements are products between two neighbours.
+
+function newArr(arr){
+    let arrNew = [];
+    for(let i = 0; i<arr.length-1; i++){
+        arrNew.push(arr[i]*arr[i+1])
+    }
+    return arrNew
+}
+let result = newArr([1,2,3,3,10])
+console.log(result);
+//6. Given a string and symbols. Change first symbol by the second one in the string.
+
+function newStr(str,symbol1,symbol2){
+    let strNew = '';
+    for(let i = 0;i<str.length; i++){
+        if(str[i]!=symbol1){
+            strNew+=str[i]
+        }else{
+            strNew+=symbol2
+        }
+    }
+    return strNew
+}
+let result = newStr('Barev harut','h','H')
+console.log(result);
+
+//7. Print the following number pattern:
+
+function printNum(num){
+    let arr=[]
+    let k=2*num-2
+    let num1=''
+    for(let i = 1; i<=num; i++){
+        num1 += i;
+        arr[i-1]=num1
+        arr[k--]=num1
+    }
+    let print =''
+    //return arr
+    for(let i = arr.length; i>0; i--){
+        print+=arr.pop()+'\n'
+    }
+  return print
+}
+let result = printNum(9)
 console.log(result)
