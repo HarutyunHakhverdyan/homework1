63e71658a9c5c374ac09845953cdf66dd2fd6d88
HarutyunHakhverdyan <92093271+HarutyunHakhverdyan@users.noreply.github.com>
2022-04-14 18:57:39 +0400

    Add files via upload

--- new.js
+++ new.js
@@ -1136,4 +1136,672 @@ if(car1.spid()>car2.spid()){
 	car2.show()
 }s
 */
-alert( document.body.constructor.name )
+//for(let i in document.body.childNodes){
+//	alert(i)
+//};
+/*
+let n =prompt();
+let count=0
+let result
+for(let i =2;count!=n;i++){
+	let k=1
+	for(let j =2; j<i/2+1;j++){
+		k*=i%j
+	}
+	if(k){
+		result=i
+		count++
+	}
+}
+console.log(result)
+*/
+/*
+function is(arr){
+	let newArr=[];
+	for(let i =0; i<arr.length;i++){
+		let k =1;
+		for(let j=0;j<=newArr.length;j++){
+			if(newArr[j]==arr[i]){
+				k=0
+			}
+		}
+		if(k){
+			newArr.push(arr[i])
+		}
+	}
+	return newArr
+}
+let result = is([1,2,2,3,3,5,4,7,4,58,9]);
+console.log(result)
+*/
+/*//1. Given an array․ Compute the length of the array. (without using .length)
+let arr1 = [1,2,3,4,5,7,,,undefined,,,74,75];
+let length1=0
+for(let key in arr1){
+	alert(key)
+	length1=+key+1
+};
+// ete  zangvacum bollor tarrer@ dasavorvac en hajordakan ev tarr@ undefind@ chi
+let arr2 = [1,2,3,4,'Hello',65,76,87];
+let length2=0
+for(let i = 0;arr2[i]!=undefined; i++ ){
+	length2++
+};
+console.log(`arr1-${length1}, arr2-${length2}`) 
+//2 Given an array of numbers. Print the sum of the elements in array.
+let arr = [1,8,9,12]
+let sum=0
+for(let i = 0;arr[i]!=undefined; i++ ){
+	sum += arr[i]
+};
+console.log(sum)
+//3. Given two numbers. Print powers of 2 between that numbers. (without using
+//Math.pow)
+let num1 = +prompt();
+let num2 = +prompt();
+for(let i = 0; 2**i<num2; i++ ){
+	if (2**i>num1) {
+	console.log(2**i)
+ }
+}
+let num = prompt()
+let num1=''
+for(let i = 0; i<num.length; i++){
+	if(!(num[i]%2==0&&num[i+1]%2==0)){
+       num1+=num[i]
+	} else {
+		num1+=num[i]+'-'
+	}
+}
+console.log(num1)
+//5.Insert a n positive number. Print the n-st prime number.
+let num = +prompt()
+let count = 0
+let result
+for(let num1 = 2 ;count!==num; num1++){
+let x=1
+for(let i =2; i<=num1/2+1;i++){
+   x*=num1%i
+}
+if(x||num1==2){
+	result=num1
+	++count
+ }
+}
+console.log(result)*/
+
+/*
+function hh(name) {
+    name=name+name
+	alert(name)
+};
+let  name = 'har';
+alert(name)
+hh();
+alert(name)
+name=5
+
+
+function showMessage(from, text = "no text given") {
+  alert( from + ": " + text );
+}
+
+showMessage("Ann",);
+showMessage() // Ann: no text given
+*/
+/*
+function arr(arr1,arr2){
+	arr3=[];
+	for(let i =0;i<arr1.length;i++){
+         arr3.push(arr1[i])
+     }
+     for(let j=0;j<arr2.length;j++){
+     	let k=1;
+     	for(let i =0;i<arr1.length;i++){
+     		if(arr1[i]!=arr2[j]){
+     			k=k*j
+     		}else{
+     			k*=0
+     		} 
+     	}
+     	if(k){
+     		arr3.push(arr2[j])
+     	}
+     }
+    return arr3
+}
+let result=arr([1,3,4,8],[1,2,3,4,5,6,7,8]);
+console.log(result);*/
+/*
+let result=[]
+function arr(arr1){
+    for(let i =0;i<arr1.length;i++){
+        let x=1
+        for(let j =0;j<arr1.length;j++){
+             if(arr1[i] ==arr1[j]){
+                 x=0
+             }
+         
+          }
+          if(x){
+            result.push(arr1[i])
+          }
+    }
+}
+console.log(arr([1,1,2,2,3,3,4,4]))
+*/
+/*
+let chapter = document.getElementsByTagName('span');
+
+chapter.style.background='red'*/
+/*
+function diag(matr){
+	let matDiag=[]
+	let elem=document.getElementById('divid');
+	 	elem.innerHTML+='<div id="ss"></div>'
+	 	let r=document.getElementById('ss')
+	 	
+	 for(let i=0;i<matr.length;i++){
+	 	let z=0
+	 	matDiag.push(matr[i][i])
+	 	let r=document.getElementById('ss')
+	 	r.innerHTML+=`<div id="ss2">${matr[z][i]}</div>`
+
+	}
+	let k =elem.innerHTML
+	for(let j=0;j<5;j++){
+	 		elem.innerHTML+=k
+	 	}
+	return matDiag
+};
+let matrix=[ 
+    [1, 2, 3, 4, 5], 
+    [6, 7, 8, 9, 10], 
+    [11, 12, 13, 14, 15], 
+    [16, 17, 18, 19, 20], 
+    [21, 22, 23, 24, 25], 
+]
+let result=diag(matrix)
+let elem=document.getElementById('divid');
+
+  function createparagraph() {
+            let x = document.createElement("p");
+            let t = 
+                document.createTextNode(result);
+            x.appendChild(t);
+            document.getElementById("divid").appendChild(x);
+        }
+
+*/
+//7. Print the following number pattern:
+/*
+function printNum(num){
+    let arr=[]
+    let k=2*num-2
+    let num1=''
+    for(let i = 1; i<=num; i++){
+        num1 += i;
+        arr[i-1]=num1
+        arr[k--]=num1
+    }
+    let print =''
+    //return arr
+    for(let i = arr.length; i>0; i--){
+        print+=arr.pop()+'\n'
+    }
+  return print
+}
+let result = printNum(9)
+console.log(result)
+*/
+/*
+function kent(num){
+	let ken=''
+	for(let i =2;i<num;i++){
+		let k=1
+		for(let j=2;j<i/2+1;j++){
+			if(i%j==0){
+				k=0
+			}
+		}
+			if(k||i==2){
+				ken+=i+','
+			
+		}
+	}
+	return ken
+}
+let x=+prompt()
+let result=kent(x);
+console.log(result)*/
+/*
+function Covid(name,hand,hospit,varak,mah,buj){
+    this.name=name;
+	this.handipum=hand;
+	this.hospital=hospit;  // hivandutyan @ntacq@
+	this.varak=varak;
+	this.mah=mah;
+	this.buj=buj;
+};
+const hay=new Covid('erevan',4,0.4,0.7,0.3,0.7);
+const rus=new Covid('rastov',6,0.6,0.8,0.1,0.9);
+function klinika(x,or){
+	let varakakvac=0;
+	let hospiacvac=0;
+	let mahacac=0;
+	let bujvac=0;
+  let skizb=x;
+ for(let i=0;i<or;i++){
+  let depqOrum=skizb*this.handipum;
+  let pacientOrum=depqOrum*this.varak;
+  let hospitalOrum=pacientOrum*this.hospital;
+  let mahacacOr=hospitalOrum*this.mah;
+  let bujvacOr=hospitalOrum-mahacacOr;
+  skizb=pacientOrum-hospitalOrum
+  varakakvac+=pacientOrum;
+  hospiacvac+=hospitalOrum;
+  mahacac+=mahacacOr;
+  bujvac+=bujvacOr;
+ }
+ alert(this.name+'='+"varakvel en--"+varakakvac+'hosptalacvel en--'+hospiacvac+'mahacel en--'+mahacac+'bujvel en'+bujvac)
+};
+klinika.call(hay,4,7); // skzbic ev qani or e heto
+klinika.call(rus,23,3)*/
+/*
+let elem = document.querySelectorAll('#hh1')[0]
+elem.closest('.hh').style.background='red'
+alert(elem.matches('#hh1'))*/
+/*
+let user1 = {
+	name:'har',
+	age:23
+};
+let user2 = {
+	name:'john',
+	age:32
+};
+let clon ={};
+ clon=Object.assign(clon,user1,user2);
+console.log(clon)*/
+/*
+function func(){
+	
+	let result=' i am '+this.name+' '+this.surname+this.age
+	for(let i=0; i<this.hobbies.length;i++){
+			result+=this.hobbies[i]
+	}
+		return result
+	};
+	function addHobby(hobby){
+		this.hobbies.push(hobby)
+	}
+let har= {
+	name:'Harut',
+	surname:"Hakhverdyan",
+	age:29,
+	hobbies:['sport','pictures',],
+	func,
+	addHobby,
+	
+};
+let gar = {
+	name:'garik',
+	surname:'Azatyan',
+	age:23,
+	hobbies:['football','tenis'],
+	func,
+}
+har.addHobby('bareb')
+console.log(har.func())*/
+/*
+let gar = {
+	name:'Garik',
+	surname:'Azatyan',
+	age:23,
+	hobbies:['football','tenis'],
+}
+let garCopy = {};
+for(let key in gar){
+	garCopy[key]=gar[key]
+}
+garCopy.name='Suren';
+garCopy.hobbies.push(voleibol)
+*/
+/*
+function Car(name,model,v0,a,){ //v0-skzbnakan aragutyun
+	this.name=name;				//a aragacum
+	this.model=model;
+	this.v0=v0;
+	this.a=a;
+	
+}
+const car1 = new Car('BMW','x5',10,2)
+const car2 = new Car('Mercedes','GLE',11,1.5);
+function comparison(t){
+	let s1=car1.v0*t+car1.a*t**2/2    //s1 car1 ancac chanaparh
+	let s2=car2.v0*t+car2.a*t**2/2    //s2 car2 chanaparh
+	let result=""
+	if(s1>s2){
+		result=car1.name+' '+car1.model+"--"+s1+'m'
+		console.log(result)
+	}else if(s2>s1){
+	    result=car2.name+' '+car2.model+"--"+s2+'m'
+		console.log(result)
+	}else{
+		console.log("car1 & car2"+'--'+s1+'m')
+	}	
+}
+let t =+prompt()                    //t--jam
+comparison(t)
+*/
+/*
+function Cons(name){
+	this.name=name;
+	zoo()
+}
+function zoo(){
+	alert(this.name)
+}
+
+let person = {
+	name:'har'
+}
+let person1 = {
+	name:'gar'
+}
+person.a=zoo;
+person1.b=zoo;
+
+new Cons('pavel').b=zoo
+alert(new Cons('pavel'))
+*/
+/*
+function Phone(model,foto,memory,color){
+	this.model = model;
+	this.foto = foto;
+	this.memory = memory;
+	this.color = color;
+	this.hist=[];
+	this.description = function(){
+		 let result = this.model +this.color+this.memory+this.foto;
+         console.log(result)
+	}
+	this.kont={
+		John:'055445566',
+		name2:'055665544'
+		}
+	this.call=function(){
+		let num=prompt()
+		let nameKont=''
+		for(let key in this.kont){
+		if(num==this.kont[key]){
+		    nameKont=key
+		}
+	  }
+	  if(nameKont){
+	  	alert(nameKont)
+	  }	else {
+	  	alert(num)
+	  }
+	    
+		this.hist.push(nameKont||num)
+		
+	}
+	this.addKont=function(newName,newNum){
+		let x=1
+		for(let k in this.kont){
+		if(k==newName){
+           x=0
+		}
+	}
+	if(x){
+		this.kont[newName]=newNum
+	}else{
+		this.kont[newName]=newNum
+	 }
+	};
+}
+let phon1 = new Phone('IP 12 ',' 12mp ',128,' gold ')
+phon1.description()
+phon1.addKont('har','666')
+phon1.addKont('garik','777')
+phon1.addKont('garik','888')
+phon1.addKont('garik','656')
+phon1.call()
+phon1.call()
+phon1.addKont('garik','888')
+console.log(phon1.kont)*/
+/*
+function Car(model,color,k,gasSeat){
+	this.model=model;
+	this.color=color;
+	this.engine=engine;
+	this.gearBox=gearBox;
+	this.gasStove=gasStove;
+	this.speed=speed;
+	this.k=k;
+	this.gasSeat=gasSeat
+	
+     
+};
+function engine(startStop){
+	if(startStop=='start'){
+		console.log(this.model+' engine is runing')
+		return true
+	}else if (startStop=='stop'){
+		console.log(this.model+' engine is not runing')
+		return false
+	}
+}
+function gearBox(seat){
+    if(seat=='+'){
+    	this.k+=1
+    	console.log(this.k)
+    }else if(this.seat=='-'){
+    	this.k-=1
+    	console.log(this.k)
+    }
+    return this.k
+}
+function gasStove(seatGas){
+      if(this.egine){
+      	if(seatGas=='+'){
+      		this.gasSeat+=5
+      	}else if(seatGas=='-'&&this.gasSeat-5>=0){
+      		this.gasSeat-=5
+      	}
+
+      }else{
+      	console.log(' engine is not runing')
+      }
+      return this.gasSeat
+}
+function speed(){
+
+}
+let car1=new Car('06','blue',0,0)
+car1.engine('start')
+console.log(car1.engine());
+*/
+/*
+function Car(color,){
+	this.color=color;
+	this.name=name;
+	this.gg=function(){
+		return 5
+	}
+}
+let car1=new Car('kapuytt',) 
+function name(a){
+	if(a){
+		return true
+	}else{
+		return false
+	}
+}
+function model(){
+	return 7
+}
+car1.gg()
+console.log(car1.gg)*/
+/*
+function Covid(name,hand,hospit,varak,mah,buj){
+    this.name=name;
+	this.handipum=hand;
+	this.hospital=hospit;  // hivandutyan @ntacq@
+	this.varak=varak;
+	this.mah=mah;
+	this.buj=buj;
+};
+const hay=new Covid('erevan',4,0.4,0.7,0.3,0.7);
+const rus=new Covid('rastov',6,0.6,0.8,0.1,0.9);
+function klinika(x,or){
+	let varakakvac=0;
+	let hospiacvac=0;
+	let mahacac=0;
+	let bujvac=0;
+  let skizb=x;
+ for(let i=0;i<or;i++){
+  let depqOrum=skizb*this.handipum;
+  let pacientOrum=depqOrum*this.varak;
+  let hospitalOrum=pacientOrum*this.hospital;
+  let mahacacOr=hospitalOrum*this.mah;
+  let bujvacOr=hospitalOrum-mahacacOr;
+  skizb=pacientOrum-hospitalOrum
+  varakakvac+=pacientOrum;
+  hospiacvac+=hospitalOrum;
+  mahacac+=mahacacOr;
+  bujvac+=bujvacOr;
+ }
+ alert(this.name+'='+"varakvel en--"+varakakvac+'hosptalacvel en--'+hospiacvac+'mahacel en--'+mahacac+'bujvel en'+bujvac)
+};
+klinika.call(hay,2,2); // skzbic ev qani or e heto
+klinika.call(rus,23,3)
+*/
+/*
+function Konstructr(name){
+	this.name=function (){
+		return name
+	}
+};
+function name1(){
+	console.log(this.name())
+}
+let obj=new Konstructr('gar')
+obj.name1=name1;
+obj.name1()*/
+/*
+function Car(model,color,gear,gasSeat){
+	this.model=model;
+	this.color=color;
+	this.engine=engine;
+	this.gearBox=gearBox;
+	this.gasStove=gasStove;
+	this.speed=speed;
+	this.gear=gear;
+	this.gasSeat=gasSeat
+	this.eng=true
+	
+     
+};
+function engine(startStop){
+	if(startStop=='start'){
+		console.log(this.model+' engine is runing')
+		return this.eng=true
+	}else if (startStop=='stop'){
+		console.log(this.model+' engine is not runing')
+		return this.eng=false
+	}
+}
+function gearBox(seat){
+    if(seat=='+'){
+    	this.gear+=1
+    	console.log(this.gear)
+    }else if(this.seat=='-'){
+    	this.gear-=1
+    	console.log(this.gear)
+    }
+    return this.gear
+}
+function gasStove(seatGas){
+      if(this.eng){
+      	if(seatGas=='+'){
+      		return this.gasSeat+=5
+      	}else if(seatGas=='-'&&this.gasSeat-5>=0){
+      	   	this.gasSeat-=5
+      	}
+
+      }else{
+      	console.log(' engine is not runing')
+      }
+      return this.gasSeat
+}
+function speed(){
+	let vMax =0
+  if(this.eng){
+  	if(this.gear>0&&this.gear<=5){
+  		if(this.gasSeat>0&&this.gasSeat<=10)
+  		vMax=this.gear**(3/2)*this.gasSeat**(3/2)
+  	}
+  }
+   console.log(vMax)
+}
+let car1=new Car('06','blue',0,0)
+car1.engine('start')
+car1.gearBox('+')
+car1.gearBox('+')
+car1.gearBox('-')
+console.log(car1.gasStove('+'));
+car1.gasStove('+')
+car1.speed()*/
+/*
+let arr=[11,22,33,44,55,66,77,88,99]
+arr['func']=func
+function func(start,number,elem){
+	let arr1 =[]
+	let k=0
+	let t=0
+	debugger
+
+    for(let i =0; i<this.length;i++){
+       
+        if(i>=start&&i<start+number){
+       	 arr1.push(this[i])
+       }
+       
+    }
+    let p=this.length
+    
+    for(let i =0;i<p;i++){
+    	if(i=!start){
+    		this[i]=this[i]
+    	}else if(i>=start+number){
+    		this[start++]='barev'
+    	}
+    }
+    return arr1
+}
+console.log(arr)
+console.log(arr.func(2,3))*/
+let arr5=[11,222,33,44,555,66,777,888,999];
+arr5[fakeSplice]=fakeSplice
+function fakeSplice(start, deletecount, element1) {
+    let deletedElements = [];
+    for(let i = start; i < start + deletecount; i++){
+        deletedElements.push(this[i]);
+    }
+    for (let i = start; i < this.length; i++) {
+        if(i === start){
+            this[i] = element1;
+            i++
+        }
+        this[i] = this[i + deletecount - 1];
+    }
+    for (let j = 0; j < deletecount - 1; j++) {
+        this.pop();
+    }
+    return deletedElements;
+}
+
+console.log(arr5.fakeSplice(2,4))
+console.log(arr5)
